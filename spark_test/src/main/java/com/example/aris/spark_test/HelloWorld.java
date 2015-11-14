1282a9ad30d103cf0ac9a5f7fceaeadc18a28a81
ARIS CRUZ <dcrow14@gmail.com>
2015-11-14 21:09:08 +0800

    initial commit

---
+++ spark_test/src/main/java/com/example/aris/spark_test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.aris.spark_test;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		get("/hello", (req, res) -> "Hello Fucker");
+	}
+
+}
