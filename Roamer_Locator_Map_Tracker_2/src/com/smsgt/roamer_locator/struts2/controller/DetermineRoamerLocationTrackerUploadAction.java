a4dc82fd00d396440bad0709c03a79df7f21d656
Master_A2Z <Master_A2Z>
2016-04-30 10:30:42 +0800

    bugfix in adhoc upload local when file content is ',xxxxx'; code optimization

--- Roamer_Locator_Map_Tracker_2/src/com/smsgt/roamer_locator/struts2/controller/DetermineRoamerLocationTrackerUploadAction.java
+++ Roamer_Locator_Map_Tracker_2/src/com/smsgt/roamer_locator/struts2/controller/DetermineRoamerLocationTrackerUploadAction.java
@@ -8,7 +8,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.TimeZone;
 
 import org.apache.commons.io.FileUtils;
@@ -34,26 +36,28 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 	private String uploadFileFileName;
 	private String jsonResult;
 	private String isInternationalRP;
-	private StringBuilder sb;
-	private String operatorImsiTemp = "", /*currentImsiOperator = "",*/ operatorMsisdnTemp = ""/*, currentMsisdnOperator = ""*/;
-	//private boolean isImsiOperatorSet = false, isMsisdnOperatorSet = false;
+	private StringBuilder sbValid, sbInvalid;
+	private String operatorImsiTemp = "", operatorMsisdnTemp = "";
 	
 	private boolean isValuesValid = false, hasInvalidChars = false;
 	private String sessionUserId;
 	
 	private static final Logger logger = Logger.getLogger(DetermineRoamerLocationTrackerUploadAction.class);
 	private MapTrackerPropertiesReader mapTrackerPropertiesReader = new MapTrackerPropertiesReader("maptracker.properties");
+	private Set<String> msisdnSet = new HashSet<String>();
 	
 	
 	public String parseUploadedFile() {
 		
 			try {
 				
+				initializeMsisdnMap();
+				
 				String adhocFileDir = mapTrackerPropertiesReader.getValueFromProperty("adhocDir");
 				String adhocFileName = mapTrackerPropertiesReader.getValueFromProperty("adhocFileName");
-				//long delay = Long.parseLong(mapTrackerPropertiesReader.getValueFromProperty("adhocDelay"));
 				
-				sb = new StringBuilder();
+				sbValid = new StringBuilder();
+				sbInvalid = new StringBuilder();
 		
 				try {
 					// try to use Thread.sleep to create a delay
@@ -111,81 +115,28 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 									} 
 								}
 								
-								//if(!isImsiOperatorSet && !isMsisdnOperatorSet) { // no base operators yet
-								/*	if(!imsi.equalsIgnoreCase("NULL") || !msisdn.equalsIgnoreCase("NULL")) {
-										// get first 5 digits of imsi to determine what operator to process
-										if(Boolean.valueOf(isInternationalRP)) {
-											if(!imsi.equalsIgnoreCase("NULL")) {
-												currentImsiOperator = imsi.substring(0,3);
-												
-												if(!currentImsiOperator.equalsIgnoreCase("515")) {
-													operatorImsiTemp = currentImsiOperator;	
-													validateEntriesForImsi(operatorImsiTemp, currentImsiOperator, imsi, msisdn, vlr);
-													isImsiOperatorSet = true;
-													isValuesValid = true;
-												} else {
-													isValuesValid = false;
-												}
-												
-											} if(!msisdn.equalsIgnoreCase("NULL")) {
-												currentMsisdnOperator = msisdn.substring(0, 5);
-												if(!currentMsisdnOperator.equalsIgnoreCase("63917")) {
-													operatorMsisdnTemp = currentMsisdnOperator;	
-													validateEntriesForMsisdn(operatorMsisdnTemp, currentMsisdnOperator, imsi, msisdn, vlr);
-													isMsisdnOperatorSet = true;
-													isValuesValid = true;
-												} else {
-													isValuesValid = false;
-												}
-											}
-										} else {
-											if(!imsi.equalsIgnoreCase("NULL")) {
-												currentImsiOperator = imsi.substring(0,3);
-												
-												if(currentImsiOperator.equalsIgnoreCase("515")) {
-													operatorImsiTemp = currentImsiOperator;	
-													validateEntriesForImsi(operatorImsiTemp, currentImsiOperator, imsi, msisdn, vlr);
-													isImsiOperatorSet = true;
-													isValuesValid = true;
-												} else {
-													isValuesValid = false;
-												}
-												
-											} if(!msisdn.equalsIgnoreCase("NULL")) {
-												currentMsisdnOperator = msisdn.substring(0, 5);
-												if(currentMsisdnOperator.equalsIgnoreCase("63917")) {
-													operatorMsisdnTemp = currentMsisdnOperator;	
-													validateEntriesForMsisdn(operatorMsisdnTemp, currentMsisdnOperator, imsi, msisdn, vlr);
-													isMsisdnOperatorSet = true;
-													isValuesValid = true;
-												} else {
-													isValuesValid = false;
-												}
-											}
-										}	
-									} */
-								//} else { // if already set
-									
-									if(isValuesValid) {
-										if(!imsi.equalsIgnoreCase("NULL")) {
-											
-											String operatorToCompare = imsi.substring(0,3);
-											validateEntriesForImsi(operatorImsiTemp, operatorToCompare, imsi, msisdn, vlr);
-											
-										} else if(!msisdn.equalsIgnoreCase("NULL")) {
-											
-											String operatorToCompare = msisdn.substring(0,5);
-											validateEntriesForMsisdn(operatorMsisdnTemp, operatorToCompare, imsi, msisdn, vlr);
-											
-										}
-									} 
-								//}		
+								if(isValuesValid) {
+									if(!imsi.equalsIgnoreCase("NULL")) {
+										
+										String operatorToCompare = imsi.substring(0,3);
+										validateEntriesForImsi(operatorImsiTemp, operatorToCompare, imsi, msisdn, vlr);
+										
+									} else if(!msisdn.equalsIgnoreCase("NULL")) {
+										
+										String operatorToCompare = msisdn.substring(0,5);
+										validateEntriesForMsisdn(operatorMsisdnTemp, operatorToCompare, imsi, msisdn, vlr);
+										
+									}
+								} 	
 							}
 						}
 						br.close();
-						if(isValuesValid) {
+						
+						// proceed only if sbInvalid does not have value
+						logger.debug("parseUpload2 -> sbInvalid contents: " + sbInvalid.toString());
+						if(sbInvalid.toString().trim().isEmpty()) {
 							boolean sftpSuccess = false;
-							FileUtils.writeStringToFile(file, sb.toString());		
+							FileUtils.writeStringToFile(file, sbValid.toString());		
 							sftpSuccess = pushFileToServer(file);	
 							//sftpSuccess = true;
 							if(sftpSuccess) {
@@ -242,8 +193,6 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 				
 				operatorImsiTemp = "";
 				operatorMsisdnTemp = "";
-				//isImsiOperatorSet = false;
-				//isMsisdnOperatorSet = false;
 				isValuesValid = true;
 				hasInvalidChars = false;
 				
@@ -258,11 +207,13 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 		
 		try {
 			
+			initializeMsisdnMap(); 
+			
 			String adhocFileDir = mapTrackerPropertiesReader.getValueFromProperty("adhocDir");
 			String adhocFileName = mapTrackerPropertiesReader.getValueFromProperty("adhocFileName2");
-			//long delay = Long.parseLong(mapTrackerPropertiesReader.getValueFromProperty("adhocDelay"));
 			
-			sb = new StringBuilder();
+			sbValid = new StringBuilder();
+			sbInvalid = new StringBuilder();
 	
 			try {
 				// try to use Thread.sleep to create a delay
@@ -319,82 +270,31 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 									imsi = lContent[0];
 								} 
 							}
-							
-							//if(!isImsiOperatorSet && !isMsisdnOperatorSet) { // no base operators yet
-							/*	if(!imsi.equalsIgnoreCase("NULL") || !msisdn.equalsIgnoreCase("NULL")) {
-									// get first 5 digits of imsi to determine what operator to process
-									if(Boolean.valueOf(isInternationalRP)) {
-										if(!imsi.equalsIgnoreCase("NULL")) {
-											currentImsiOperator = imsi.substring(0,3);
-											
-											if(!currentImsiOperator.equalsIgnoreCase("515")) {
-												operatorImsiTemp = currentImsiOperator;	
-												validateEntriesForImsi(operatorImsiTemp, currentImsiOperator, imsi, msisdn, vlr);
-												isImsiOperatorSet = true;
-												isValuesValid = true;
-											} else {
-												isValuesValid = false;
-											}
-											
-										} if(!msisdn.equalsIgnoreCase("NULL")) {
-											currentMsisdnOperator = msisdn.substring(0, 5);
-											if(!currentMsisdnOperator.equalsIgnoreCase("63917")) {
-												operatorMsisdnTemp = currentMsisdnOperator;	
-												validateEntriesForMsisdn(operatorMsisdnTemp, currentMsisdnOperator, imsi, msisdn, vlr);
-												isMsisdnOperatorSet = true;
-												isValuesValid = true;
-											} else {
-												isValuesValid = false;
-											}
-										}
-									} else {
-										if(!imsi.equalsIgnoreCase("NULL")) {
-											currentImsiOperator = imsi.substring(0,3);
-											
-											if(currentImsiOperator.equalsIgnoreCase("515")) {
-												operatorImsiTemp = currentImsiOperator;	
-												validateEntriesForImsi(operatorImsiTemp, currentImsiOperator, imsi, msisdn, vlr);
-												isImsiOperatorSet = true;
-												isValuesValid = true;
-											} else {
-												isValuesValid = false;
-											}
-											
-										} if(!msisdn.equalsIgnoreCase("NULL")) {
-											currentMsisdnOperator = msisdn.substring(0, 5);
-											if(currentMsisdnOperator.equalsIgnoreCase("63917")) {
-												operatorMsisdnTemp = currentMsisdnOperator;	
-												validateEntriesForMsisdn(operatorMsisdnTemp, currentMsisdnOperator, imsi, msisdn, vlr);
-												isMsisdnOperatorSet = true;
-												isValuesValid = true;
-											} else {
-												isValuesValid = false;
-											}
-										}
-									}	
-								} */
-							//} else { // if already set
-								
-								if(isValuesValid) {
-									if(!imsi.equalsIgnoreCase("NULL")) {
-										
-										String operatorToCompare = imsi.substring(0,3);
-										validateEntriesForImsi(operatorImsiTemp, operatorToCompare, imsi, msisdn, vlr);
-										
-									} else if(!msisdn.equalsIgnoreCase("NULL")) {
-										
-										String operatorToCompare = msisdn.substring(0,5);
-										validateEntriesForMsisdn(operatorMsisdnTemp, operatorToCompare, imsi, msisdn, vlr);
-										
-									}
+
+							if(isValuesValid) {
+								if(!imsi.equalsIgnoreCase("NULL")) {
+									
+									String operatorToCompare = imsi.substring(0,3);
+									validateEntriesForImsi(operatorImsiTemp, operatorToCompare, imsi, msisdn, vlr);
+									
 								} 
-							//}		
+								
+								if(!msisdn.equalsIgnoreCase("NULL")) {
+									
+									String operatorToCompare = msisdn.substring(0,5);
+									validateEntriesForMsisdn(operatorMsisdnTemp, operatorToCompare, imsi, msisdn, vlr);
+									
+								}
+							} 	
 						}
 					}
+					
 					br.close();
-					if(isValuesValid) {
+					// proceed only if sbInvalid does not have value
+					logger.debug("parseUpload2 -> sbInvalid contents: " + sbInvalid.toString());
+					if(sbInvalid.toString().trim().isEmpty()) {
 						boolean sftpSuccess = false;
-						FileUtils.writeStringToFile(file, sb.toString());		
+						FileUtils.writeStringToFile(file, sbValid.toString());		
 						//sftpSuccess = pushFileToServer(file);	
 						sftpSuccess = true;
 						if(sftpSuccess) {
@@ -451,8 +351,6 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 			
 			operatorImsiTemp = "";
 			operatorMsisdnTemp = "";
-			//isImsiOperatorSet = false;
-			//isMsisdnOperatorSet = false;
 			isValuesValid = true;
 			hasInvalidChars = false;
 			
@@ -461,107 +359,95 @@ public class DetermineRoamerLocationTrackerUploadAction extends ActionSupport {
 			jsonResult = JSONConverterUtility.convertToJSONFormat("Upload failed! Error in uploaded file");
 		}
 		return ActionSupport.SUCCESS; 				
-}	
-	
-	
+	}	
 	
 	public void validateEntriesForImsi(String operatorImsiTemp, String currentImsiOperator, String imsi, String msisdn, String vlr) {
 		
 		if(Boolean.valueOf(isInternationalRP)) {
 			
-			//if(currentImsiOperator.equalsIgnoreCase(operatorImsiTemp)) {
-				if(!imsi.startsWith("515")) {
-					msisdn = "NULL";
-					sb.append(imsi).append(",")
-					.append(msisdn).append(",")
-						.append(vlr).append(",")
-							.append("NULL,NULL\n");	
-					isValuesValid = true;
-				} else {
-					isValuesValid = false;
-				}
-			//}
-			
+			if(!imsi.startsWith("515")) {
+				msisdn = "NULL";
+				sbValid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+				isValuesValid = true;
+			} else {
+				msisdn = "NULL";
+				sbInvalid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+				isValuesValid = false;
+			}
 		} else {
-			//if(currentImsiOperator.equalsIgnoreCase(operatorImsiTemp)) {
-				if(imsi.startsWith("515")) {
-					msisdn = "NULL";
-					sb.append(imsi).append(",")
-					.append(msisdn).append(",")
-						.append(vlr).append(",")
-							.append("NULL,NULL\n");	
-					isValuesValid = true;
-				} else {
-					isValuesValid = false;
-				}
-			//}
+			
+			if(imsi.startsWith("515")) {
+				msisdn = "NULL";
+				sbValid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+				isValuesValid = true;
+			} else {
+				msisdn = "NULL";
+				sbInvalid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+				isValuesValid = false;
+			}
 		}
 		
 	}
 	
 	public void validateEntriesForMsisdn(String operatorMsisdnTemp, String currentMsisdnOperator, String imsi, String msisdn, String vlr) {
 		
-		if(Boolean.valueOf(isInternationalRP)) {
-			
-			List<String> localMsisdnList = Arrays.asList("0813","0907","0908","0909","0910","0912","0918","0919","0920","0921","0928","0929","0930","0938","0939","0946","0947","0948","0949","0989","0998","0999","0922","0923","0925","0932","0933","0934","0942","0943","0817","0905","0906","0915","0916","0917","0926","0927","0935","0936","0937","0994","0996","0997","0977","0979","0973","0974","0975"
-					,"63913","63907","63908","63909","63910","63912","63918","63919","63920","63921","63928","63929","63930","63938","63939","63946","63947","63948","63949","63989","63998","63999","63922","63923","63925","63932","63933","63934","63942","63943","63817","63905","63906","63915","63916","63917","63926","63927","63935","63936","63937","63994","63996","63997","63977","63979","63973","63974", "63975");
-			
-			//if(currentMsisdnOperator.equalsIgnoreCase(operatorMsisdnTemp)) {
-			//if(localMsisdnList.contains(msisdn)) {
-				for(String s: localMsisdnList) {
-					if(!msisdn.startsWith(s)) {
-						isValuesValid = true;	
-					} else {
-						isValuesValid = false;
-					}
-				}
-				
-				if(isValuesValid) {
-					sb.append(imsi).append(",")
-					.append(msisdn).append(",")
-						.append(vlr).append(",")
-							.append("NULL,NULL\n");	
-				}
-			//}
-				
-			//}
+		String msisdnPrefix = "";
+		if (msisdn.startsWith("0")) {
+			msisdnPrefix = msisdn.substring(0,4);
+		} else if (msisdn.startsWith("63")){
+			msisdnPrefix = msisdn.substring(0,5);
+		}
+		
+		if(Boolean.valueOf(isInternationalRP)) {	
+	
+			if (msisdnSet.contains(msisdnPrefix)) {
+				sbValid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+			} else {
+				sbInvalid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+			}
 			
 		} else {
 			
-			List<String> localMsisdnList = Arrays.asList("0813","0907","0908","0909","0910","0912","0918","0919","0920","0921","0928","0929","0930","0938","0939","0946","0947","0948","0949","0989","0998","0999","0922","0923","0925","0932","0933","0934","0942","0943","0817","0905","0906","0915","0916","0917","0926","0927","0935","0936","0937","0994","0996","0997","0977","0979","0973","0974","0975"
-					,"63913","63907","63908","63909","63910","63912","63918","63919","63920","63921","63928","63929","63930","63938","63939","63946","63947","63948","63949","63989","63998","63999","63922","63923","63925","63932","63933","63934","63942","63943","63817","63905","63906","63915","63916","63917","63926","63927","63935","63936","63937","63994","63996","63997","63977","63979","63973","63974","63975");
-			
-			//if(currentMsisdnOperator.equalsIgnoreCase(operatorMsisdnTemp)) {
-			//if(localMsisdnList.contains(msisdn)) {
-				for(String s: localMsisdnList) {
-					if(msisdn.startsWith(s)) {
-						isValuesValid = true;	
-					} else {
-						isValuesValid = false;
-					}
-				}
-				
-				if(isValuesValid) {
-					sb.append(imsi).append(",")
-					.append(msisdn).append(",")
-						.append(vlr).append(",")
-							.append("NULL,NULL\n");	
-				}
-			//}
-			
-			//if(currentMsisdnOperator.equalsIgnoreCase(operatorMsisdnTemp)) {
-			//	if(msisdn.startsWith("639") || msisdn.startsWith("09")) {
-			//		sb.append(imsi).append(",")
-			//		.append(msisdn).append(",")
-			//			.append(vlr).append(",")
-			//				.append("NULL,NULL\n");	
-			//		isValuesValid = true;
-			//	} else {
-			//		isValuesValid = false;
-			//	}
-			//}
+			if (msisdnSet.contains(msisdnPrefix)) {
+				sbValid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+			} else {
+				sbInvalid.append(imsi).append(",")
+				.append(msisdn).append(",")
+					.append(vlr).append(",")
+						.append("NULL,NULL\n");	
+			}
 		}
+	}
+	
+	private void initializeMsisdnMap() {
 		
+		List<String> localMsisdnList = Arrays.asList("0813","0907","0908","0909","0910","0912","0918","0919","0920","0921","0928","0929","0930","0938","0939","0946","0947","0948","0949","0989","0998","0999","0922","0923","0925","0932","0933","0934","0942","0943","0817","0905","0906","0915","0916","0917","0926","0927","0935","0936","0937","0994","0996","0997","0977","0979","0973","0974","0975"
+				,"63913","63907","63908","63909","63910","63912","63918","63919","63920","63921","63928","63929","63930","63938","63939","63946","63947","63948","63949","63989","63998","63999","63922","63923","63925","63932","63933","63934","63942","63943","63817","63905","63906","63915","63916","63917","63926","63927","63935","63936","63937","63994","63996","63997","63977","63979","63973","63974", "63975");
+		
+		for (String s: localMsisdnList) {
+			msisdnSet.add(s);
+		}
 	}
 	
 	public boolean pushFileToServer(File file) {
